922df499b5e076951f281a577899a21fd18951bd
Abastro <abab9579@gmail.com>
2025-11-23 15:50:39 +0900

    Back to level naming

--- src/core/levelled.rs
+++ src/core/levelled.rs
@@ -1,46 +1,45 @@
 pub const MAX_MODULUS_COUNT: usize = 64;
 
-/// Denotes a group of basis modulus.
+/// Denotes a level as group of basis modulus.
 /// RNS decomposition is used to represent large modulus,
 /// where the modulus is split into "basis" moduli.
 #[derive(Clone, Copy)]
-pub struct ModGroup {
+pub struct Level {
     modulus_count: usize,
     used_modulus: [usize; MAX_MODULUS_COUNT],
 }
 
-impl ModGroup {
+impl Level {
+    /// Modulus
     fn used_modulus(&self) -> &[usize] {
         &self.used_modulus[..self.modulus_count]
     }
 }
 
-impl PartialEq for ModGroup {
+impl PartialEq for Level {
     fn eq(&self, other: &Self) -> bool {
         self.used_modulus() == other.used_modulus()
     }
 }
 
 /// Handle for levels, allowing for easier conversion between certain modulus.
-pub trait ModGroupHandle<Mod> {
+pub trait LevelHandle<Mod> {
     /// The list of modulus factors of the current modulus group.
     fn get_modulus(&self) -> &Vec<Mod>;
 
-    fn bottom_level(&self) -> ModGroup;
+    fn bottom_level(&self) -> Level;
 
     /// Denotes the top level excluding the auxiliary modulus.
     /// The auxiliary modulus cannot appear in encryptions, so we exclude it in the usual levels.
-    fn top_level(&self) -> ModGroup;
+    fn top_level(&self) -> Level;
 
     /// 'One level' higher than the given level.
     /// Gives None if the higher level does not exist.
-    fn higher_level(&self, level: ModGroup) -> Option<ModGroup>;
+    fn higher_level(&self, level: Level) -> Option<Level>;
 
     /// 'One level' lower than the given level.
     /// Gives None if the lower level does not exist.
-    fn lower_level(&self, level: ModGroup) -> Option<ModGroup>;
-
-    // TODO How to handle base extension?
+    fn lower_level(&self, level: Level) -> Option<Level>;
 }
 
 /// Verbose
@@ -57,51 +56,51 @@ const LINEARS: [usize; MAX_MODULUS_COUNT] = {
 // TODO Auxiliary modulus for linear level handling
 
 /// Linear level handling with auxiliary modulus.
-pub struct LinearModHandle<Mod> {
+pub struct LinearLevelHandle<Mod> {
     modulus: Vec<Mod>,
     max_level: usize,
 }
 
-impl<Mod> LinearModHandle<Mod> {
-    pub fn new(modulus: Vec<Mod>, max_level: usize) -> LinearModHandle<Mod> {
+impl<Mod> LinearLevelHandle<Mod> {
+    pub fn new(modulus: Vec<Mod>, max_level: usize) -> LinearLevelHandle<Mod> {
         assert!(modulus.len() <= MAX_MODULUS_COUNT);
-        LinearModHandle { modulus, max_level }
+        LinearLevelHandle { modulus, max_level }
     }
 
-    pub fn get_level(&self, level: usize) -> Option<ModGroup> {
-        (level <= self.max_level).then(|| ModGroup {
+    pub fn get_level(&self, level: usize) -> Option<Level> {
+        (level <= self.max_level).then(|| Level {
             modulus_count: level + 1,
             used_modulus: LINEARS,
         })
     }
 
-    pub fn level_usize(&self, level: ModGroup) -> Option<usize> {
+    pub fn level_usize(&self, level: Level) -> Option<usize> {
         (level.used_modulus == LINEARS)
             .then(|| level.modulus_count - 1)
             .filter(|lv| *lv <= self.max_level)
     }
 }
 
-impl<Mod> ModGroupHandle<Mod> for LinearModHandle<Mod> {
+impl<Mod> LevelHandle<Mod> for LinearLevelHandle<Mod> {
     fn get_modulus(&self) -> &Vec<Mod> {
         &self.modulus
     }
 
-    fn bottom_level(&self) -> ModGroup {
+    fn bottom_level(&self) -> Level {
         self.get_level(0).expect("bottom level should be valid")
     }
 
-    fn top_level(&self) -> ModGroup {
+    fn top_level(&self) -> Level {
         self.get_level(self.max_level)
             .expect("top level should be valid")
     }
 
-    fn higher_level(&self, level: ModGroup) -> Option<ModGroup> {
+    fn higher_level(&self, level: Level) -> Option<Level> {
         self.level_usize(level)
             .and_then(|lv| self.get_level(lv + 1))
     }
 
-    fn lower_level(&self, level: ModGroup) -> Option<ModGroup> {
+    fn lower_level(&self, level: Level) -> Option<Level> {
         self.level_usize(level)
             .and_then(|lv| self.get_level(lv - 1))
     }
