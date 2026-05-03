4c5df67688e603a486f31d58dd668d5ec7e03163
Abastro <abab9579@gmail.com>
2026-05-03 23:20:55 +0900

    Cyclotomic products

--- src/algebra/poly/cyclop2.rs
+++ src/algebra/poly/cyclop2.rs
@@ -1,8 +1,9 @@
 use crate::{
     algebra::poly::cyclotomic::Cyclo,
-    engine::handle::AddGroupHandle,
+    engine::handle::{AddGroupHandle, RingHandle},
     tensor::{tensor::TensorShape, tensor_ops::PointwiseHandle},
-    utils::math::powers::is_pow2,
+    the_debug,
+    utils::math::powers,
 };
 
 /// Denotes the standard power-of-two cyclotomic ring, `Z[X]/(X^N + 1)`.
@@ -13,7 +14,7 @@ pub struct CycloP2 {
 
 impl CycloP2 {
     pub fn new(degree: usize) -> Self {
-        assert!(is_pow2(degree as u64));
+        assert!(powers::is_pow2(degree as u64));
         CycloP2 { degree }
     }
 }
@@ -51,8 +52,9 @@ impl<H> CycloP2Handle<H> {
     }
 }
 
-// TODO Implement vectorization
+// TODO Implement vectorization for Mod64
 
+/// Generic additive implementation
 impl<H, T> AddGroupHandle<[T]> for CycloP2Handle<&H>
 where
     H: AddGroupHandle<T>,
@@ -90,23 +92,56 @@ where
     }
 }
 
-// TODO inefficient h_mul
+impl<H> CycloP2Handle<&H> {
+    /// Computes negacyclic convolution in Z[X]/(X^N + 1).
+    pub fn compute_mul<T: Clone>(&self, lhs: &[T], rhs: &[T]) -> Vec<T>
+    where
+        H: RingHandle<T>,
+    {
+        let degree = crate::the_debug!(lhs.len(), rhs.len());
+        debug_assert!(powers::is_pow2(degree as u64));
 
-// impl<T, H> RingHandle<[T]> for CycloP2Handle<&H>
-// where
-//     H: RingHandle<T>,
-// {
-//     fn h_set_one(&self, val: &mut [T]) {
-//         for entry in val {
-//             self.handle_scalar.h_set_one(entry);
-//         }
-//     }
+        let mut out: Vec<T> = lhs.to_vec();
+        for entry in &mut out {
+            self.handle_scalar.h_set_zero(entry);
+        }
 
-//     fn h_mul(&self, lhs: &[T], rhs: &[T], out: &mut [T]) {
-//         todo!()
-//     }
+        for i in 0..degree {
+            for j in 0..degree {
+                let mut tmp = rhs[j].clone();
+                self.handle_scalar.h_mul_assign(&mut tmp, &lhs[i]);
+                let target = (i + j) % degree;
+                if i + j < degree {
+                    self.handle_scalar.h_add_assign(&mut out[target], &tmp);
+                } else {
+                    // X^N = -1: wrap folds back
+                    self.handle_scalar.h_sub_assign(&mut out[target], &tmp);
+                }
+            }
+        }
 
-//     fn h_mul_assign(&self, lhs: &mut [T], rhs: &[T]) {
-//         todo!()
-//     }
-// }
+        out
+    }
+}
+
+/// Less efficient generic implementation of cyclotomic product
+impl<T: Clone, H> RingHandle<[T]> for CycloP2Handle<&H>
+where
+    H: RingHandle<T>,
+{
+    fn h_set_one(&self, val: &mut [T]) {
+        for entry in val {
+            self.handle_scalar.h_set_one(entry);
+        }
+    }
+
+    fn h_mul(&self, lhs: &[T], rhs: &[T], out: &mut [T]) {
+        let result = self.compute_mul(lhs, rhs);
+        out.clone_from_slice(&result[..]);
+    }
+
+    fn h_mul_assign(&self, lhs: &mut [T], rhs: &[T]) {
+        let result = self.compute_mul(lhs, rhs);
+        lhs.clone_from_slice(&result[..]);
+    }
+}
